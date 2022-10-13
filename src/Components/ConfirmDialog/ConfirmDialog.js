43b1a8cb98203ee159fb4e9d54ae24ebf4563cec
Koray Karakoyun <koraykarakoyun@hotmail.com>
2022-10-13 19:39:34 +0300

    confirm dialog(onay butonu) eklendi

--- src/Components/ConfirmDialog/ConfirmDialog.js
+++ src/Components/ConfirmDialog/ConfirmDialog.js
@@ -5,39 +5,45 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useEffect } from 'react';
 
 export default function ConfirmDialog(props) {
     const [open, setOpen] = React.useState(false);
-
     const handleClickOpen = () => {
         setOpen(true);
     };
-
     const handleClose = () => {
         setOpen(false);
     };
 
+    useEffect(() => {
+        props.setIsConfirm(false)
+    })
     return (
         <div>
             <Button variant="outlined" onClick={handleClickOpen}>
-                Submit
+                {props.buttonName}
             </Button>
             <Dialog
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
+                aria-describedby="alert-dialog-description">
                 <DialogTitle id="alert-dialog-title">
-                    Are You Sure
+                    Dikkat!
                 </DialogTitle>
-
+                <DialogContent>
+                    Islemi kaydetmek istediginize emin misiniz?
+                </DialogContent>
                 <DialogActions>
                     <Button onClick={() => {
-                        props.handleSubmission()
+                        props.setIsConfirm(true)
                         handleClose()
-                    }}>Yes</Button>
-                    <Button autoFocus onClick={handleClose}>No</Button>
+                    }}>Evet</Button>
+                    <Button onClick={() => {
+                        props.setIsConfirm(false)
+                        handleClose()
+                    }}>Hayır</Button>
                 </DialogActions>
             </Dialog>
         </div>
--- src/Components/FileUpload/FileUpload.js
+++ src/Components/FileUpload/FileUpload.js
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { api } from "../../Utilities/Api"
 import ConfirmDialog from '../ConfirmDialog/ConfirmDialog';
 
@@ -8,28 +9,28 @@ export default function FileUpload(props) {
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
     const [IsSelected, setIsSelected] = useState(false);
+    const [isConfirm, setIsConfirm] = useState(false);
     const changeHandler = (event) => {
         setSelectedFile(event.target.files[0]);
         setIsSelected(true);
 
     };
 
+    useEffect(() => {
+        console.log(isConfirm);
+        if (isConfirm) {
 
-    const handleSubmission = () => {
-
-        const formData = new FormData();
-        formData.append('file', selectedFile);
-        formData.append('fileName', selectedFile.name);
-        formData.append('ProductId',props.productId);
-        api("POST", "localhost", "7098", "products", "upload", null, formData).then(res => console.log(res));
-
-    };
+            const formData = new FormData();
+            formData.append('file', selectedFile);
+            formData.append('fileName', selectedFile.name);
+            formData.append('ProductId', props.productId);
+            api("POST", "localhost", "7098", "products", "upload", null, formData).then(res => console.log(res));
+        }
+    })
 
     return (
         <div>
-
             <input type="file" name="file" onChange={changeHandler} />
-
             {IsSelected ? (
                 <div>
                     <p>Filename: {selectedFile.name}</p>
@@ -42,7 +43,7 @@ export default function FileUpload(props) {
                 </div>
             ) : null}
             <div>
-                <button onClick={handleSubmission}>Submit</button>
+                <ConfirmDialog buttonName="Resmi Kaydet" setIsConfirm={setIsConfirm}></ConfirmDialog>
             </div>
         </div>
     )
